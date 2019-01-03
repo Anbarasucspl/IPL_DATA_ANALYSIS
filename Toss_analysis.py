ee13162533480d043012ba306b0c4d700afa3632
Anbarasucspl <anbarasu.mahadev@gmail.com>
2019-01-03 12:04:43 +0530

    Update Toss_analysis.py

--- Toss_analysis.py
+++ Toss_analysis.py
@@ -3,11 +3,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 class ipl_analysis:
     def bar_grph(self,team_names, no_of_match_toss_win,no_of_match_indiv,Tot_indiv_mat_win,Year_input):
-        fig_size=plt.figure(figsize=(20, 15))
+        fig_size=plt.figure(figsize=(20, 10))
         bar_width = 0.25
         y_pos = np.arange(len(team_names)) 
         result1=plt.bar(y_pos, no_of_match_toss_win,bar_width,color='b', alpha=1, label='Toss_win_num')
-        result2=plt.bar(y_pos+bar_width,no_of_match_indiv,bar_width,color='r',alpha=1, label='Total_no_num')
+        result2=plt.bar(y_pos+bar_width,no_of_match_indiv,bar_width,color='r',alpha=1, label='Indiv_no_Match')
         result3=plt.bar(y_pos+bar_width+bar_width,Tot_indiv_mat_win,bar_width,color='g',alpha=1, label='Total_indiv_win')
         plt.ylabel('WON_NUMBERS')
         plt.xlabel('IPL_TEAM_NAMES')
@@ -15,25 +15,31 @@ class ipl_analysis:
         plt.xticks(y_pos+bar_width, team_names)
         plt.legend()
         plt.show()
-
+    def pie_graph(self,totl_bat_field):
+        label_field=['bat','field']
+        plt.pie(totl_bat_field,labels=label_field,startangle=90,autopct='%.1f%%')
+        plt.title('TOSS_DECISION')
+        plt.show()
 Year_input=str(input())
 csv_file = open("D:\Python\ipl\matches.csv",'r')
 csv_data = csv.DictReader(csv_file)
-no_of_match_indiv=[]
+no_of_match_indiv=[] 
 no_of_match_toss_win=[]
 Toss_parti_Teams=[]
 Toss_won_team=[]
 Match_won=[]
 Tot_indiv_mat_win=[]
-data_2014={}
+Toss_decision=[]
 for read_1 in sorted(csv_data):
     #team_names=read_1.keys()
     if read_1['season']== Year_input:
         Toss_won_team.append(read_1['toss_winner'])
         Toss_parti_Teams.append([read_1['team1'],read_1['team2']])
         Match_won.append(read_1['winner'])
+        Toss_decision.append(read_1['toss_decision'])
 Total_matches=len(Toss_won_team)
 team_names=set(Toss_won_team)
+totl_bat_field=[Toss_decision.count('bat'),Toss_decision.count('field')]
 for team in set(Toss_won_team):
     counter=0
     counter_win=0
@@ -52,3 +58,4 @@ for team in set(Toss_won_team):
     Tot_indiv_mat_win.append(counter_mat_win)
 object_ipl=ipl_analysis()
 object_ipl.bar_grph(team_names,no_of_match_toss_win,no_of_match_indiv,Tot_indiv_mat_win,Year_input)
+object_ipl.pie_graph(totl_bat_field)
